5e03ff7fa6c8f1e23fc9c5fd965c6f27c82c88a1
AbelardoOchoa <109985159+AbelardoOchoa@users.noreply.github.com>
2022-11-15 16:41:47 -0500

    ok

---
+++ code_to_run.py
@@ -0,0 +1,66 @@
+from codigo import Usuario, Pelicula
+from time import sleep
+user = Usuario()
+sw = True
+pelicula1 = Pelicula("Duro de matar", ["2:30 PM", "10:10 AM", 
+                    "5:00 PM"], 15, 10)
+pelicula2 = Pelicula("Buscando a Nemo", ["2:30 PM", "10:10 AM", 
+                    "5:00 PM"], 20, 15)
+pelicula3 = Pelicula("Avatar", ["2:30 PM", "10:10 AM", 
+                    "5:00 PM"], 10, 20)
+pelicula1.vaciar_acientos()
+pelicula2.vaciar_acientos()
+pelicula3.vaciar_acientos()
+
+def menu_peli():
+    """
+    Despliega las películas disponibles y permite escoger una.
+    """
+    print(f"Hola, {user.nombre_usuario}, qué quieres ver hoy?")
+    print(f"1. {pelicula1.titulo}")
+    print(f"2. {pelicula2.titulo}")
+    print(f"3. {pelicula3.titulo}")
+    opcion = int(input())
+    if opcion == 1:
+        pelicula1.vender_ticket()
+    elif opcion == 2:
+        pelicula2.vender_ticket()
+    elif opcion == 3:
+        pelicula3.vender_ticket()
+    else:
+        print("Opcion invalida.")
+        menu_peli()
+    
+def menu_inicial():
+    """
+    Despliega el menú principal y permite elegir una opción.
+    """
+    print("Bienvenido al teatro.")
+    print("Digite una opción:")
+    print("1. Ingresar.")
+    print("2. Registrarse.")
+    print("3. Salir")
+    opcion = int(input())
+    while(opcion < 1 or opcion > 3):
+        print("Bienvenido al teatro.")
+        print("Digite una opción:")
+        print("1. Ingresar.")
+        print("2. Registrarse.")
+        print("3. Salir")
+        opcion = int(input())
+    if opcion == 1:
+        if user.sin_usuarios:
+            print("Aún no ha creado ningún usuario, ", end="")
+            print("regístrese primero.")
+            input("Presione enter para volver al menú principal.")
+        else:   
+            user.ingresar()
+            menu_peli()
+    elif opcion == 2:
+        user.registrar()
+    elif opcion == 3:
+        print("Adios, hasta pronto.")
+        sleep(1)
+        exit()
+while sw:
+    menu_inicial()
--- codigo.py
+++ codigo.py
@@ -1,3 +1,4 @@
+from time import sleep
 class Usuario():
     def __init__(self) -> None:
         self.nombre_usuario = str
@@ -5,39 +6,55 @@ class Usuario():
         self.usuarios = []
         self.password = []
         self.preferencia = int
+        self.sin_usuarios = True
     
     def registrar(self) -> None:
+        """
+        Permite que los usuarios registrados puedan ingresar.
+        """
         print("¡Hola, bienvenido a registro!")
         print("Por favor, digite un nombre de usuario:")
         self.nombre_usuario = input()
         if self.nombre_usuario in self.usuarios:
             while self.nombre_usuario in self.usuarios:
-                print("Digite otro nombre, este ya existe.")
+                print("Digite otro nombre, este ya existe:")
+                self.nombre_usuario = input()
             self.usuarios.append(self.nombre_usuario)
         else:
             self.usuarios.append(self.nombre_usuario)
         print("Por favor, digite una contraseña:")
         self.contraseña = input()
-        self.nombre_usuario = input()
+        self.password.append(self.contraseña)
         print("Digite el número de asiento de su preferencia.")
         self.preferencia = input()
         print("¡Registro exitoso!")
-        input("Presione enter.")
+        self.sin_usuarios = False
+        input("Presione enter para continuar.")
         
     def ingresar(self) -> None:
+        """
+        Permite crear uno o más usuarios.
+        """
         print("¡Hola, bienvenido a ingreso!")
         print("Por favor, digite su nombre de usuario:")
         self.nombre_usuario = input()
+        while self.nombre_usuario not in self.usuarios:
+            print("No existe este nombre de usuario, por favor \
+                digite otro:")
+            self.nombre_usuario = input()
         if self.nombre_usuario in self.usuarios:
             print("Por favor, digite una contraseña:")
             self.contraseña = input()
+        if self.contraseña in self.password:
+            print("¡Ingreso exitoso!")
+            sleep(1.3)
+        else:
+            while self.contraseña not in self.password:
+                print("Contraseña incorrecta, intenta de nuevo")
+                self.contraseña = input()
             if self.contraseña in self.password:
                 print("¡Ingreso exitoso!")
-            else:
-                while self.contraseña not in self.password:
-                    print("Contraseña incorrecta, intenta de nuevo")
-                    self.contraseña = input()
-                    
+                sleep(1.3)
 
 class Asiento():
     def __init__(self) -> None:
@@ -52,7 +69,7 @@ class Tickete():
         self.tipo = int
                 
 class Pelicula(Tickete, Asiento):
-    def __init__(self, titulo: str, hora: str, regular: int,
+    def __init__(self, titulo: str, hora: list, regular: int,
                  vip: int) -> None:
         self.titulo = titulo
         self.hora = hora
@@ -60,41 +77,64 @@ class Pelicula(Tickete, Asiento):
         self.vip = vip
         self.cantidad = self.regular + self.vip
         self.disponibles = self.cantidad
+        self.asientos = []
     
     def vender_ticket(self) -> None:
+        """
+        Este método permite que el usuario adquiera sus entradas.
+        """
+        print("Escoja un horario.")
+        print(f"1. {self.hora[0]}")
+        print(f"2. {self.hora[1]}")
+        print(f"3. {self.hora[1]}")
+        horario = input()
         print("Digite el tipo de entrada que desea comprar.")
         print("Digite 1 para regular o 2 para VIP")
-        self.tipo = int(input)
-        numero_tickets = int(input("Por favor digite la cantidad \
-            de entradas que desea comprar."))
+        self.tipo = int(input())
+        print("Por favor digite la cantidad " +
+            "de entradas que desea comprar.")
+        numero_tickets = int(input())
         if numero_tickets <= self.disponibles:
             self.disponibles -= numero_tickets
-            print("Compra realizada.")
         else: 
-            print(f"Lo sentimos, no hay esa cantidad de entradas \
-                disponibles. Quedan {numero_tickets} entradas")
+            print("Lo sentimos, no hay esa cantidad de entradas " +
+                f"disponibles. Quedan {numero_tickets} entradas")
             while numero_tickets > self.disponibles:
-                numero_tickets = int(input("Por favor digite la \
-                    cantidad de entradas que desea comprar."))
+                numero_tickets = int(input("Por favor digite la " +
+                    "cantidad de entradas que desea comprar."))
             self.disponibles -= numero_tickets
+            if self.tipo == 1:
+                self.regular -= 1
+            elif self.tipo == 2:
+                self.vip -= 1
         print("Escoja los asientos que desee:")  
         self.escoger_asiento(numero_tickets)  
         print("Compra realizada.")
+        sleep(1)
+        print("Disfrute la película.")
+        sleep(1)
         
     def escoger_asiento(self, comprados) -> None:
+        """
+        Permite que el usuario escoja los asientos de su preferencia.
+        """
         aux = 0
         num_asiento = int
         while(aux < comprados):
-            print("Digite un número de tickete:")
+            print("Digite un número de asiento:")
+            num_asiento = int(input())
             if num_asiento in self.asientos:
-                for j in range(self.asientos):
+                for j in range(len(self.asientos)):
                     if self.asientos[j] == num_asiento:
-                        self.num_asiento[j] = 0
+                        self.asientos[j] = 0
                         aux += 1
             else:
                 print("Asiento no disponible.")
             
     
     def vaciar_acientos(self) -> None:
+        """
+        Permite que todos los puestos estén disponibles.
+        """
         for i in range(self.cantidad):
             self.asientos.append(i+1)
